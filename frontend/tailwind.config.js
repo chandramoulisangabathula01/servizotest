dc9c0ca551c0c4b9c6257f02afa4f9e6c15e709a
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2025-02-16 18:16:17 +0530

    frontend website deployment

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      "./public/**/*.html",
+      // Add other file paths if you have them
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FBC452', // Your primary color
+        },
+      },
+    },
+    plugins: [],
+  }
